c944249ccf98002c80340f4971824aa55342bcb4
Allen Leis <allen@rocinante.lan>
2017-08-05 15:42:01 -0400

    adds placeholder logging mixin

--- anemoi/config.py
+++ anemoi/config.py
@@ -24,7 +24,7 @@ from confire import environ_setting
 import os
 
 ##########################################################################
-## Classes
+## Misc Configuration
 ##########################################################################
 
 class SlackConfiguration(Configuration):
@@ -32,6 +32,70 @@ class SlackConfiguration(Configuration):
 
 class DarkSkyConfiguration(Configuration):
     access_token = ""
+
+##########################################################################
+## Logging Configuration
+##########################################################################
+
+class LoggingConfiguration(Configuration):
+    """
+    Specialized configuration system for the Python logging module. After 2.7
+    Python now accepts configurations for logging from a dictionary. This
+    configuration class exposes that dictionary on demand to the logging
+    system. Note that it is complex because of the nested, internal configs.
+    """
+    version = 1
+    disable_existing_loggers = False
+
+    formatters = {
+        'simple': {
+            'format': '[%(asctime)s] %(levelname)s {%(name)s.%(funcName)s:%(lineno)d} %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S %z',
+        }
+    }
+
+    handlers = {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'logfile': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'logs/anemoi.log',
+            'maxBytes': '67108864',
+            'formatter': 'simple',
+        }
+    }
+
+    loggers =  {
+        'anemoi': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'logfile'],
+            'propagagte': True,
+        },
+        'py.warnings': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'logfile'],
+            'propagate': True,
+        }
+    }
+
+    def dict_config(self):
+        """
+        Returns the dictionary configuration for use with the Python logging
+        module's logging.config.dictConfigClass function.
+        """
+        return dict(self.options())
+
+##########################################################################
+## App Configuration
+##########################################################################
 
 class DefaultConfiguration(Configuration):
 
@@ -41,6 +105,7 @@ class DefaultConfiguration(Configuration):
 
     slack = SlackConfiguration()
     dark_sky = DarkSkyConfiguration()
+    logging = LoggingConfiguration()
 
     zip_code = 20001
 
